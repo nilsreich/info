9032e265070a46b42169df592d1234407735bdd3
nilsreich <nilsreich@gmail.com>
2023-07-24 11:44:49 +0000

    init

---
+++ theme.config.jsx
@@ -0,0 +1,32 @@
+export default {
+  logo: <span>Informationsverarbeitung</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra'
+  },
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="https://nextra.site" target="_blank">
+          Nextraasd
+        </a>
+        .
+      </span>
+    )
+  },
+  feedback: {
+    content: null
+  },
+  editLink: {
+    text: null
+  },
+  project: {
+    link: null
+  },
+  toc: {
+    title: (<span>Inhaltsverzeichnis</span>)
+  },
+  search: {
+    placeholder: 'Suche'
+  },
+}
